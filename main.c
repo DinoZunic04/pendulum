fcf9b28d94fa76372a28394e7282131401daba4d
DinoZunic04 <46324462+DinoZunic04@users.noreply.github.com>
2019-04-01 20:17:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #define g 9.80665
-#define dt 0.01
+#define dt 0.00001
 #define M_PI acos(-1.0)
 int main()
 {
 	double l,m,pocetniUgao;
-	for(pocetniUgao=20.0*M_PI/180.0;pocetniUgao<=2*M_PI;pocetniUgao+=20.0*M_PI/180.0)
+	for(pocetniUgao=0.0;pocetniUgao<=M_PI-dt;pocetniUgao+=M_PI/180.0*1.0)
 	{
-		printf("O=%d\n",(int)(pocetniUgao*180.0/M_PI));
+		printf("O=%d\n",(int)(pocetniUgao*180.0/M_PI+0.0000000001));
 		for(l=1.0;l<4.0;l+=1.0)
 		{
 			printf("l=%3dm: ",(int)l);
@@ -14,7 +14,7 @@ int main()
 			for(t=0.0;;t+=dt)
 			{
 				omega-=dt*g/l*sin(kteta);
-				if(znak(kteta)!=znak(kteta+dt*(omega-dt*g/l*sin(kteta)))) break;
+				if(dt*g/l*sin(kteta)==0.0||(znak(kteta)!=znak(kteta+dt*(omega-dt*g/l*sin(kteta))))) break;
 				kteta+=dt*omega;
 			}
 			t*=4.0;
