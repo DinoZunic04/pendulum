d6b5bc1a2a15c12955ca45d959bfe85388fea379
DinoZunic04 <46324462+DinoZunic04@users.noreply.github.com>
2019-04-01 17:23:57 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #define g 9.80665
-#define dt 0.001
+#define dt 0.01
 #define M_PI acos(-1.0)
 int main()
 {
@@ -7,18 +7,18 @@ int main()
 	for(pocetniUgao=20.0*M_PI/180.0;pocetniUgao<=2*M_PI;pocetniUgao+=20.0*M_PI/180.0)
 	{
 		printf("O=%d\n",(int)(pocetniUgao*180.0/M_PI));
-		for(l=1.0;l<=4.0;l+=1.0)
+		for(l=1.0;l<4.0;l+=1.0)
 		{
 			printf("l=%3dm: ",(int)l);
 			double t,kteta=pocetniUgao,omega=0.0;
 			for(t=0.0;;t+=dt)
 			{
-				omega+=dt*g/l*sin(kteta);
-				if(znak(kteta)!=znak(kteta+dt*(omega+dt*g/l*sin(kteta)))) break;
+				omega-=dt*g/l*sin(kteta);
+				if(znak(kteta)!=znak(kteta+dt*(omega-dt*g/l*sin(kteta)))) break;
 				kteta+=dt*omega;
 			}
 			t*=4.0;
-			printf("%lf16.10s\t",t);
+			printf("%16.10lfs\t",t);
 		}
 		printf("\n");
 	}
